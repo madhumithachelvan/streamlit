f999ebd2eb06c74befac82605d9984d3098ce3e3
HongyuChen2022 <hongyu.chen@iris.uni-stuttgart.de>
2025-02-08 05:43:38 +0100

    Unpdated app

--- survey3.py
+++ survey3.py
@@ -522,8 +522,7 @@ if "current_text_index" not in st.session_state:
 
 
 def page6():
-    col1, col2, col3 = st.columns([1, 3, 1])
-    with col2:
+
         st.header("Survey Questions")
         st.markdown(
             """
