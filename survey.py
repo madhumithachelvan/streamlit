9725c5146b3fa694825f37acd2f4f5ad949ee470
madhumithachelvan <167061320+madhumithachelvan@users.noreply.github.com>
2025-06-14 13:46:28 +0200

    Update survey.py

--- survey.py
+++ survey.py
@@ -642,7 +642,7 @@ def page6():
 
         # Enable the "Next" button only if both fields are filled
         if st.button("Next", disabled=not (is_confidence_selected and is_slider_selected)):
-            for t in range(3):
+            for t in range(2):
                 time.sleep(.2)
             #   st.session_state["responses"][current_index]["confidence"] = None
             if current_index < len(data) - 1:
