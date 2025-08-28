ca13b70162817289d1e55dc98ce6151597e964f0
madhumithachelvan <madhu.chelvan@gmail.com>
2025-08-28 13:41:02 +0200

    merge annotations and compare old and new scores

---
+++ reannotations/merged.py
@@ -0,0 +1,58 @@
+import glob
+import pandas as pd
+import os
+
+responses_dir = 'reannotations'
+
+count = 0
+
+file_list = []
+
+file_list = glob.glob("survey_responses*.csv",root_dir=responses_dir)
+file_list = [os.path.join(responses_dir,file) for file in file_list]
+
+dataframes = [pd.read_csv(f) for f in file_list]
+merged_df = pd.concat(dataframes, ignore_index=True)
+
+# Write the merged DataFrame to a new CSV file
+merge_name = os.path.join(responses_dir,f"merged_reannotations.csv")
+
+merged_df.to_csv(merge_name, index=False)
+
+
+# for i,folder in enumerate(os.listdir(responses_dir)):
+#     if folder not in ['.DS_Store', 'merged.py', 'merged']:
+
+#         folder_path = os.path.join(responses_dir,folder)
+#         if len(os.listdir(folder_path)) == 0:
+#             continue
+
+#         # Find all CSV files starting with "survey_responses"
+#         if folder == 'demographic':
+#             file_list = glob.glob("prolific_export*.csv",root_dir=folder_path)
+#         else:
+#             file_list = glob.glob("survey_responses*.csv",root_dir=folder_path)
+#         file_list = [os.path.join(folder_path,file) for file in file_list]
+#         file_list.sort()  # Optional: sort the list
+
+#         # Read each CSV file into a DataFrame and concatenate them
+#         dataframes = [pd.read_csv(f) for f in file_list]
+#         merged_df = pd.concat(dataframes, ignore_index=True)
+
+#         # Write the merged DataFrame to a new CSV file
+#         if folder == 'demographic':
+#             merge_name = os.path.join(responses_dir,f"merged/merged_demographics.csv")
+#         else:
+#             merge_name = os.path.join(responses_dir,f"merged/merged_pilot{folder}_responses.csv")
+
+#         merged_df.to_csv(merge_name, index=False)
+#         count += 1  # Keeps count of response folders
+
+
+
+
+
+
+
+
+
